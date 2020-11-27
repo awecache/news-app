d93167205d7903d071c030055be453fcf7a17e6d
Steven <stevenzh2020@gmail.com>
2020-11-27 09:35:49 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NewsComponent } from './components/news/news.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: 'countries', component: CountryListComponent },
+  { path: 'news', component: NewsComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    NewsComponent,
+    CountryListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
